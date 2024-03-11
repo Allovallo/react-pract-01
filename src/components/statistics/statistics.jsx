035043df2ddef4c28bb06ab9d245d3035735d5e1
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-03-11 21:55:35 +0200

    files updated

---
+++ src/components/statistics/statistics.jsx
@@ -0,0 +1,11 @@
+import css from './profile.module.css'
+import PropTypes from 'prop-types';
+
+export const StatisticalItem = () => {
+    return (
+        <ListItem>
+            <Label></Label>
+            <Percentage></Percentage>
+        </ListItem>
+    )
+}
