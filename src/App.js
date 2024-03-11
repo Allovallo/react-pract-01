17db407c2a9606952b4fda654fc7281456aeedaa
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-03-11 16:09:37 +0200

    profile updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import user from './startData/user.json';
+import { Profile } from 'components/profile/profile';
+
 function App() {
   return (
     <div>
-      App
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 }
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -8,31 +8,49 @@ export const UserDescription = ({name, tag, location, avatar}) => {
                 alt='Аватар користувача'
                 className={css.avatar}
             />
-            <p className={css.name}></p>
-            <p className={css.tag}></p>
-            <p className={css.location}></p>
+            <p className={css.name}>{ name }</p>
+            <p className={css.tag}>@{ tag }</p>
+            <p className={css.location}>{ location }</p>
         </div>
     )
 }
 
-export const Profile = () => {
+UserDescription.defaultProps = {
+    avatar: 'https://www.flaticon.com/svg/static/icons/svg/3784/3784184.svg',
+}
+
+export const Profile = ({name, tag, location, avatar, stats}) => {
     return (
-        <div>
-            <UserDescription></UserDescription>
-            <ul>
-                <li>
-                    <span></span>
-                    <span></span>
+        <div className={css.profile}>
+            <UserDescription
+                name={name}
+                tag={tag}
+                location={location}
+                avatar={avatar}
+                stats={stats}
+            />
+            <ul className={css.statList}>
+                <li className={css.statElem}>
+                    <span className={css.label}>Followers</span>
+                    <span className={css.quantity}>{ stats.followers }</span>
                 </li>
-                                <li>
-                    <span></span>
-                    <span></span>
+                <li className={css.statElem}>
+                    <span className={css.label}>Views</span>
+                    <span className={css.quantity}>{ stats.views }</span>
                 </li>
-                <li>
-                    <span></span>
-                    <span></span>
+                <li className={css.statElem}>
+                    <span className={css.label}>Likes</span>
+                    <span className={css.quantity}>{ stats.likes }</span>
                 </li>
             </ul>
         </div>
     )
+}
+
+Profile.defaultProps = {
+    stats: {
+        followers: 0,
+        views: 0,
+        likes: 0,
+    }
 }
